f07f88d9bca2c27e51152e01e97979f1f104527c
jedi_yoda <imoyarou007@gmail.com>
2021-08-02 02:54:52 +0900

    revise python code

--- CommonCode.py
+++ CommonCode.py
@@ -7,6 +7,9 @@ import pandas_profiling
 import lightgbm as lgb
 import optuna
 from sklearn.metrics import log_loss
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 
@@ -35,7 +38,11 @@ from sklearn.model_selection import train_test_split
 
 # 訓練データの読み込み
 train = pd.read()
+# 訓練データから目的変数を抜いたテーブル
+# x_trainを作成する
 x_train = train.drop(['目的変数'],axis = 1)
+# 訓練データから目的変数だけの配列
+# y_trainを作成する
 y_train = train['目的変数']
 
 # テストデータの読み込み
@@ -45,7 +52,8 @@ x_test = test.copy()
 
 # 単純に訓練データと検証データに分ける場合
 # train_test_splitを用いる
-X_train, X_valid, y_train, t_valid = train_test_split(X_train, y_train,
+# kfoldを用いるときはその都度validを作成するので下のコードは実行しない
+X_train, X_valid, y_train, y_valid = train_test_split(x_train, y_train,
                                                     test_size=0.3,
                                                     random_state=42,
                                                     stratify=y_train)
@@ -59,6 +67,8 @@ X_train, X_valid, y_train, t_valid = train_test_split(X_train, y_train,
 scores_accuracy = []
 # logloss:モデルの性能
 scores_logloss = []
+# mean_squared_error
+scores_RMSE = []
 
 # 実行する
 kf = KFold(n_splits=4,shuffle=True,random_state=42)
@@ -71,7 +81,7 @@ for tr_idx, va_idx in kf.split(x_train):
     lgb_eval = lgb.Dataset(X_valid, y_valid, reference=lgb_train)
     # パラメータチューニングの箇所はここになる
     params = {
-    'objective'
+    'objective':'regression'
     }
     model = lgb.train(params, lgb_train,
                 valid_sets = [lgb_train, lgb_eval],
@@ -83,14 +93,18 @@ for tr_idx, va_idx in kf.split(x_train):
     # 検証データの正解と検証で予測したものを利用する
     logloss = log_loss(y_valid, y_pred_valid)
     accuracy = accuracy_score(y_valid, y_pred_valid)
+    RMSE = mean_squared_error(Y_valid, y_pred_valid)
     # そのfoldのスコアを保存する
     scores_logloss.append(logloss)
     scores_accuracy.append(accuracy)
+    scores_RMSE.append(RMSE)
 # 各foldのスコアの平均を出力する
 logloss = np.mean(scores_logloss)
 accuracy = np.mean(scores_accuracy)
 print(f'logloss: {logloss:.4f}, accuracy: {accuracy:.4f}')
 
+# 基本形
+# trainとsplitで分けたのちに行っていく行動
 # 予測器の学習から予測まで:not cross validation
 lgb_train = lgb.Dataset(X_train, y_train)
 lgb_eval = lgb.Dataset(X_valid, y_valid, reference=lgb_train)
@@ -105,6 +119,7 @@ model = lgb.train(params, lgb_train,
                 num_boost_round = 1000,
                 early_stopping_rounds = 10)
 
+# テストデータで予測値を出す
 y_pred = model.predict(X_test, num_iteration=model.best_iteration)
 # log_lossでモデルの性能を見れる
 
